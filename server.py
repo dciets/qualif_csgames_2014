54b80ccc6e03523b61b9abf4391186997868444a
Israel Halle <isra017@gmail.com>
2014-01-21 16:38:32 -0500

    Added auth command and connect/disconnect message

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python2
+
+import sqlite3
 import socket
 import threading
 import SocketServer
 import traceback
+import uuid
 
 class Singleton(type):
     _instances = {}
@@ -17,36 +20,55 @@ def extract_metadata(msg):
     if msg[0] == "[" and msg.find("]") != -1:
         content = msg[msg.find("]") + 1:].strip()
         metadata_str = msg[1:msg.find("]")]
-        metadata = {s[0].strip()[1:]:s[1].strip() for s in [i.split("=") for i in metadata_str.split(",")]}
+        metadata = {s[0].strip():s[1].strip() for s in [i.split("=") for i in metadata_str.split(",")]}
 
     return (metadata, content)
+
+def extract_credential(msg):
+    return map(str.strip, msg.split(' ')[1:])
 
 class ChatServer(object):
     __metaclass__ = Singleton
 
     clients = dict()
 
-    def register(self, client):
-        if client.username in self.clients or client.username == "server":
-            self.send_error("Username %s is already taken" % client.username, client)
-            return False
+    def __init__(self):
+        self.db_conn = sqlite3.connect('user.db')
 
-        self.clients[client.username] = client
+    def register(self, client, creds):
+        pass
 
-        print "Client '%s' connecting (%d client registered)" % (client.username, len(self.clients))
-        return True
+    def authenticate(self, client, creds):
+        # Validate credential
 
-    def unregister(self, client):
-        del self.clients[client.username]
+        username, pwd = creds
+        print 'auth', username, pwd
 
-        print "Client '%s' disconnecting (%d client registered)" % (client.username, len(self.clients))
+        if username in self.clients:
+            self.clients[username].close()
+        self.clients[username] = client
 
-    def send_all(self, msg, src, metadata={}):
+        client.is_auth = True
+        client.username = username
+
+        self.send_all('user-connect ' + client.username)
+
+        print "Client '%s' connecting (%d clients connected)" % (client.username, len(self.clients))
+
+    def disconnect(self, client):
+        if not client.was_closed:
+            del self.clients[client.username]
+
+        self.send_all('user-disconnect ' + client.username)
+
+        print "Client '%s' disconnecting (%d clients connected)" % (client.username, len(self.clients))
+
+    def send_all(self, msg, src=None, metadata={}):
         for client in self.clients.values():
             if not client is src:
                 self.send(msg, client, src)
 
-    def send_to(self, msg, dst, src, metadata={}):
+    def send_to(self, msg, dst, src=None, metadata={}):
         self.send(msg, self.clients[dst], src)
 
     def send_error(self, error, dst):
@@ -59,7 +81,7 @@ class ChatServer(object):
             dst.request.sendall(metadata_stamp + msg)
         except:
             print traceback.format_exc()
-            self.unregister(dst)
+            self.disconnect(dst)
 
     def handle_msg(self, msg, src):
         if not msg:
@@ -68,40 +90,54 @@ class ChatServer(object):
         metadata, content = extract_metadata(msg)
         if "dst" not in metadata:
             self.send_all(content, src, metadata)
-        elif metadata["dst"] != "server":
-            if metadata["dst"] in self.clients:
-                self.send_to(content, metadata["dst"], src, metadata)
-            else:
-                self.send_error("User %s is not connected" % metadata["dst"], src)
-        else:
+        elif metadata["dst"] in self.clients:
+            self.send_to(content, metadata["dst"], src, metadata)
+        elif metadata["dst"] == "server":
             self.handle_command(content, src)
+        else:
+            self.send_error("User %s is not connected" % metadata["dst"], src)
+
 
     def handle_command(self, cmd, src):
         if cmd == "list":
             self.send("\n".join(self.clients.keys()), src, metadata={"type":"user_list"})
+        elif cmd.startswith(("register", "auth")):
+            creds = extract_credential(cmd)
+            if len(creds) != 2:
+                self.send_error("Invalid parameter count. %d given, expecting 2" % len(creds), src)
+                return
+
+            if cmd.startswith("register"):
+                self.register(src, creds)
+            else:
+                self.authenticate(src, creds)
         else:
             self.send_error("Unknown command: %s" % cmd, src)
 
 class ClientHandler(SocketServer.BaseRequestHandler):
     def setup(self):
         self.server = ChatServer()
+        self.was_closed = False
+        self.username = str(uuid.uuid4())
+        self.is_auth = False
 
     def handle(self):
-        self.username = self.request.recv(32).strip()
-        if not (self.username and self.server.register(self)):
-            return
-
         try:
-            while True:
+            while not self.was_closed:
                 msg = self.request.recv(1024)
                 if not msg:
                     break
-
+                print msg
                 self.server.handle_msg(msg.strip(), self)
         except:
             print traceback.format_exc()
 
-        self.server.unregister(self)
+        self.server.disconnect(self)
+
+    def close(self):
+        self.request.close()
+        self.was_closed = True
+
 
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
